9933eecd8b0f0f7b77c63333225889557a4e4a37
corsiqx <kilian.kersten@gmx.de>
2026-06-07 16:07:51 +0200

    Added Alle Lizenzen Component with Route

---
+++ src/app/components/pages/alle-lizenzen/alle-lizenzen.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alle-lizenzen',
+  imports: [],
+  templateUrl: './alle-lizenzen.html',
+  styleUrl: './alle-lizenzen.css',
+})
+export class AlleLizenzen {
+
+}
