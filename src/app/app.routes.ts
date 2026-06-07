2ccb056010dab42345435965fba1a96ffb8e11d9
corsiqx <kilian.kersten@gmx.de>
2026-06-07 15:56:49 +0200

    Added Auslastung Component with Route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,10 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', loadComponent: () => import('./components/pages/home/home').then((m) => m.Home) },
+  {
+    path: 'auslastung',
+    loadComponent: () =>
+      import('./components/pages/auslastung/auslastung').then((m) => m.Auslastung),
+  },
   { path: '**', redirectTo: '' },
 ];
